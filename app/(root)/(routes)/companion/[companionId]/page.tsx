47907b640b7dedc5d8c13dc8389adc96b75dda2e
Serhii Sharipov <sharipov14@gmail.com>
2024-02-28 20:08:07 -0500

    Add New Companion Form

---
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -0,0 +1,24 @@
+import { CompanionForm } from "./components/companion-form";
+import prismadb from "@/lib/prismadb";
+
+interface CompanionIdPageProps {
+  params: {
+    companionId: string;
+  };
+}
+
+const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
+  // TODO: Check subscription
+
+  const companion = await prismadb.companion.findUnique({
+    where: { id: params.companionId },
+  });
+
+  const categories = await prismadb.category.findMany();
+
+  return <CompanionForm 
+  initialData={companion}
+  categories={categories}/>;
+};
+
+export default CompanionIdPage;
